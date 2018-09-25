4a055c1d838b3a326468382b0dbf0eb58b1af0c1
Mona Lisa <kjh311@gmail.com>
2018-09-25 13:27:47 -0700

    asdf

--- consumerTrack2/scripts/script.js
+++ consumerTrack2/scripts/script.js
@@ -12,20 +12,20 @@ function validateNameForm() {
 	var bornYear = yearDrop.options[yearDrop.selectedIndex].value;
 	var bornDay = dayDrop.options[dayDrop.selectedIndex].value;
 	var todaysDate = new Date().toLocaleDateString();
-	var date18YearsAgoArray = todaysDate.split("/")
-	var month18YearsAgo = parseInt(date18YearsAgoArray[0]);
-	var day18YearsAgo = parseInt(date18YearsAgoArray[1]);
-	var year18YearsAgo = parseInt(date18YearsAgoArray[2] - 18);
+	var dateArray = todaysDate.split("/")
+	var month18YearsAgo = parseInt(dateArray[0]);
+	var day18YearsAgo = parseInt(dateArray[1]);
+	var year18YearsAgo = parseInt(dateArray[2] - 18);
 	var nameFormWrapper = document.getElementById("name-form-div");
 	var emailWrapper = document.getElementById("email-form-div");
 	var errorText = document.getElementById("age-error-text");
 
-	console.log("Year born on was " + bornYear);
-	console.log("Month born on was " + bornMonth);
-	console.log("Day born on was " + bornDay);
-	console.log("today's date is " +todaysDate)
-	console.log(date18YearsAgoArray);
-	console.log("year 18 years ago was " + year18YearsAgo)
+	// console.log("Year born on was " + bornYear);
+	// console.log("Month born on was " + bornMonth);
+	// console.log("Day born on was " + bornDay);
+	// console.log("today's date is " +todaysDate)
+	// console.log(date18YearsAgoArray);
+	// console.log("year 18 years ago was " + year18YearsAgo)
 
 
 	function calculate_age(dob) { 
@@ -39,8 +39,8 @@ function validateNameForm() {
 
 	if(calculate_age(new Date(bornYear, bornMonth, bornDay)) < 18){
 		// alert("you must 18 or older to enter");
-		errorText.classList.remove("no-opacity"); 
-    	errorText.classList.add("opacity"); 
+		errorText.classList.remove("d-none"); 
+    	errorText.classList.add("d-block"); 
 		return false;
 	}
 
