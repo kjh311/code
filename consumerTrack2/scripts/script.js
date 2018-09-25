f2a23283d59d8486421f611b646524c76ddc789d
Mona Lisa <kjh311@gmail.com>
2018-09-25 07:47:23 -0700

    first fom works

--- consumerTrack2/scripts/script.js
+++ consumerTrack2/scripts/script.js
@@ -1,3 +1,11 @@
-$( document ).ready(function() {
-    // $(".dropdown-toggle").dropdown();
-});
+function validateNameForm() {
+
+	var nameFormWrapper = document.getElementById("name-form-div");
+	var emailWrapper = document.getElementById("email-form-div");
+
+	nameFormWrapper.classList.remove("d-block"); 
+    nameFormWrapper.classList.add("d-none"); 
+    emailWrapper.classList.remove("d-none"); 
+    emailWrapper.classList.add("d-block"); 
+
+}
